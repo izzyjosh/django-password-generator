70835a566b463830fab54e6548d9be4e5a1b6990
izzyjosh <calebayuba222@gmail.com>
2023-12-05 16:20:13 +0100

    added urls for newly created views

--- password_generator/password/urls.py
+++ password_generator/password/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
         path("", views.index, name="index"),
-        path("signin/", views.signin, name="signin"), 
-        path("signup/", views.signup, name="signup"), 
-        path("logout/", views.logout, name="logout"), 
+        path("signup/",views.signup,name="signup"),
+        path("signin/",views.signin,name="login"),
+        path("logout/",auth_views.LogoutView.as_view(),name="logout"),
         path("display_password/<str:username>", views.dis_pass, name="dis_pass"),
-        path("view_password/<str:username>", views.view_pass, name="view_pass"),
-        path("recover_password/", views.forgotten, name="forgotten"),
-        path("update_Password/", views.update, name="update_pass"), 
+        path("view_password/<str:username>", views.view_pass, name="view_pass"), 
         ]
