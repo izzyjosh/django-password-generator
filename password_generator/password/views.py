ffcebdbe1c74bfe56f17f455d15053095d1c377e
izzyjosh <calebayuba222@gmail.com>
2023-12-05 16:18:59 +0100

    rewrote my signup view

--- password_generator/password/views.py
+++ password_generator/password/views.py
@@ -5,12 +5,13 @@ from django.contrib import messages
 from django.contrib.auth.models import User, auth
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
-import re
 import base64
-import smtplib
-from .forms import ForgottenPassword,MyPasswordChangeForm
 from django.contrib.auth import get_user_model,update_session_auth_hash 
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
+
+
+
 
 
 #homepage view function
@@ -75,73 +76,13 @@ def index(request):
     return render(request, "index.html", )
 
 
-#view function for signup
-def signup(request):
-
-    #defining a function to validate user password
-    def validate_password(password):
-        regular_expression = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,20}$"
-        pattern = re.compile(regular_expression)
-        valid = re.search(pattern, password)
-        return valid
 
 
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        confirm_password = request.POST.get("confirm_password")
-        email = request.POST.get("email")
-
-        #validate the password using the already defin function
-        val_password = validate_password(password)
-
-        #check if the username, email exist and if the two password are equal before creatting a user
-        if not User.objects.filter(username=username).exists():
-            if not User.objects.filter(email=email).exists():
-                if password == confirm_password:
-                    if val_password:
-                        User.objects.create_user( username=username, password=password, email=email)
-                        messages.info(request, "account created successfully")
-                        return redirect("signin")
-
-                    else:
-                        messages.error(request, "Not a strong password") 
-                        return redirect("signup")
-                else:
-                    messages.error(request, "the password dont correspond")
-            else:
-                messages.error(request, "the email already exist")
-        else:
-            messages.error(request, "the username already exist")
-
-    return render(request, "signup.html") 
 
 
-#view function for signin
-def signin(request):
-
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = auth.authenticate(
-                username=username, 
-                password=password, )
-        if user is not None:
-            auth.login(request, user)
-            return redirect("view_pass", username=username)
-        
-        else:
-            messages.error(request, "incorrect username or password")
-    return render(request, "signin.html")
-
-
-def logout(request):
-	auth.logout(request)
-	return redirect("signin")
 
 
 #view function to view your generated password
-
 def dis_pass(request, username):
 
     #query the database to get the resent user who generated a password
@@ -155,8 +96,12 @@ def dis_pass(request, username):
     return render(request, "dis_pass.html", context)
 
 
+
+
+
+
 #view function to view all your generated password
-@login_required(login_url="signin")
+@login_required
 def view_pass(request, username):
 
     #a search bar that help get a particular sites password
@@ -182,51 +127,38 @@ def view_pass(request, username):
             }
     return render(request, "view_pass.html", context)
 
-@login_required(login_url="signin")
-def forgotten(request): 
+
+
+
+def signup(request):
     if request.method == "POST":
-
-        forgotten_password = ForgottenPassword(request.POST)
-        if forgotten_password.is_valid():
-            username = forgotten_password.cleaned_data["username"]
-            email = forgotten_password.cleaned_data["email"]
-            try:
-                code = "".join(random.choices("0987654321", k=6))
-                USER = "joshuajosephizzyjosh@gmail.com"
-                PASSWORD = "fdbcgktkbkebfjhm"
-
-                server = smtplib.SMTP("smtp.gmail.com", 587)
-                server.starttls()
-                server.login(USER, PASSWORD)
-                server.sendmail(USER, [email], code)
-                messages.info(request, "email sent check for you mail for code")
-
-                return redirect("insert",code=code)
-            except TimeoutError:
-                messages.error(request,"this page cannot be reached")
-                return redirect("forgotten")
-        else:
-            messages.error(request, "please validate all your input field")
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
     else:
-        forgotten_password = ForgottenPassword()
-    return render(request, "forgotten.html", {"forgotten_password":forgotten_password})
+        form = UserRegistrationForm()
+
+    return render(request,"signup.html",{"form":form})
 
 
-@login_required(login_url="signin")
-def update(request):
-    if not request.user.is_authenticated:
-        return redirect("signin")
-    else:
-        if request.method == "POST":
-            form = MyPasswordChangeForm(request.user,request.POST)
-            if form.is_valid():
-                user = form.save()
-                update_session_auth_hash(request,user)
-                messages.info(request,"successfully updated the user password ")
-                return redirect("signin")
-            else:
-                messages.error(request,"your form is invalid please try resetting it")
+
+
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = auth.authenticate(
+                username=username,
+                password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("view_pass",username)
         else:
-            form = MyPasswordChangeForm(request.user)
+            messages.error(request,"incorrect username or password")
+            return redirect("login")
 
-    return render(request, "update.html", {"form":form})
+    return render(request,"signin.html")
+
